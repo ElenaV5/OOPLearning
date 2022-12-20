5096819eefa96720012964aa72185ea3ea9afe69
ElenaV5 <elenavnev@gmail.com>
2022-12-20 22:54:14 +0500

    Added hight n width of rect

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Rect rect = new Rect();
+        rect.height = 10;
+        rect.width = 15;
+        System.out.println(rect);
+
+    }
+}
---
+++ src/Rect.java
@@ -0,0 +1,14 @@
+class Rect {
+    double width;
+    double height;
+    Rect rect;
+
+    @Override
+    public String toString() {
+        return "Rect{" +
+                "width=" + width +
+                ", height=" + height +
+                ", rect=" + rect +
+                '}';
+    }
+}
